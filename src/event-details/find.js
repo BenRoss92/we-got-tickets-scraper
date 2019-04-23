a2c751fe74c2406f1178dfd9ffa7377074322cc6
BenRoss92 <benjaminjackross@gmail.com>
2019-04-23 16:23:10 +0100

    Change function declarations for expressions

--- src/event-details/find.js
+++ src/event-details/find.js
@@ -1,6 +1,6 @@
 const cheerio = require('cheerio');
 
-function findEventType(eventPage) {
+const findEventType = eventPage => {
     const $ = cheerio.load(eventPage);
     
     const jsonLdString = $('script[type="application/ld+json"]').html();
@@ -14,7 +14,7 @@ function findEventType(eventPage) {
     )['@type'];
 }
 
-function findEventLinks(htmlDoc) {
+const findEventLinks = htmlDoc => {
     const $ = cheerio.load(htmlDoc);
     
     const eventElements = $('h2');
@@ -28,27 +28,27 @@ function findEventLinks(htmlDoc) {
     }).get();
 };
 
-function findArtists(eventPage) {
+const findArtists = eventPage => {
     const $ = cheerio.load(eventPage);
     return $('.event-information').find('h1').text();
 }
 
-function findCityAndVenue(eventPage) {
+const findCityAndVenue = eventPage => {
     const $ = cheerio.load(eventPage);
     return $('.venue-details').find('h2').text();
 }
 
-function findDate(eventPage) {
+const findDate = eventPage => {
     const $ = cheerio.load(eventPage);
     return $('.venue-details').find('h4').text();
 }
 
-function findPrice(eventPage) {
+const findPrice = eventPage => {
     const $ = cheerio.load(eventPage);
     return $('.BuyBox').find('strong').text();
 }
 
-function findEventDetails(eventPage) {
+const findEventDetails = eventPage => {
     const artists = findArtists(eventPage);
     const cityAndVenue = findCityAndVenue(eventPage);
     const date = findDate(eventPage);
