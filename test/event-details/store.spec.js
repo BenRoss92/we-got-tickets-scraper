29bd75be9fd8c5acd915369f585eede4852da23e
BenRoss92 <benjaminjackross@gmail.com>
2019-05-23 19:06:18 +0100

    Tests - use before and after blocks

--- test/event-details/store.spec.js
+++ test/event-details/store.spec.js
@@ -9,11 +9,18 @@ const createStore = require('../../src/event-details/store').createStore;
 describe('manage store of events', () => {
     describe('#createStore', () => {
         const testPath = './test/helpers/events-output.spec.csv';
+        const data = 'blah';
+        let writeFileStub;
+
+        beforeEach(() => {
+            writeFileStub = sinon.stub(fs, 'writeFile');
+        });
+
+        afterEach(() => {
+            writeFileStub.restore();
+        });
 
         it('calls fs.writeFile with the correct filepath and data', async () => {
-            const data = 'blah';
-            
-            const writeFileStub = sinon.stub(fs, 'writeFile');
             writeFileStub.yields();
             
             await createStore(fs, testPath, data);
@@ -23,29 +30,19 @@ describe('manage store of events', () => {
             expect(writeFileStub.getCall(0).args[0]).to.equal(testPath);
             expect(writeFileStub.getCall(0).args[1]).to.equal(data);
             expect(writeFileStub.getCall(0).args[2]).to.be.a('function');
-
-            writeFileStub.restore();
         });
 
         describe('when fs.writeFile is successful', () => {
             it('returns a resolved promise with no defined value', async () => {
-                const data = 'blah';
-                const writeFileStub = sinon.stub(fs, 'writeFile');
-
                 writeFileStub.yields();
                 
                 const returnValue = await createStore(fs, testPath, data);
                 expect(returnValue).to.equal(undefined);
-
-                writeFileStub.restore();
             });
         });
         
         describe('when fs.writeFile is unsuccessful', () => {
             it('returns a rejected promise with the error', async () => {
-                const data = 'blah';
-                const writeFileStub = sinon.stub(fs, 'writeFile');
-
                 const fakeError = new Error('writeFile failed');
                 
                 writeFileStub.yields(fakeError);
@@ -55,8 +52,6 @@ describe('manage store of events', () => {
                 } catch (error) {
                     expect(error).to.eql(fakeError);
                 }
-
-                writeFileStub.restore();
             });
         });
     });
