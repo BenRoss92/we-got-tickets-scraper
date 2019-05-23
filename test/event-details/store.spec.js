bef3fc23e0b15c92e79f5f50236a2f6d4ab79592
Ben Ross <benjaminjackross@gmail.com>
2019-05-23 19:30:05 +0100

    Creating the store - inject & mock the file system

--- test/event-details/store.spec.js
+++ test/event-details/store.spec.js
@@ -2,80 +2,56 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const fs = require('fs');
+const sinon = require('sinon');
 
 const createStore = require('../../src/event-details/store').createStore;
-
-const deleteIfExists = (path) => {
-    return new Promise((resolve, reject) => {
-        fs.access(path, async (err) => {
-            if (err && err.message.includes('no such file or directory')) { 
-                resolve();
-            } else if (err && !(err.message.includes('no such file or directory'))) {
-                reject(err);
-            } else {
-                await deleteStore(path);
-                resolve();
-            }
-        });
-    });
-};
-
-const readStore = (path) => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path, 'utf8', (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(data);
-            }
-        });
-    });
-};
-
-const deleteStore = (path) => {
-    return new Promise((resolve, reject) => {
-        fs.unlink(path, (err) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve();
-            }
-        });
-    });
-};
 
 describe('manage store of events', () => {
     describe('#createStore', () => {
         const testPath = './test/helpers/events-output.spec.csv';
+        const data = 'blah';
+        let writeFileStub;
 
-        beforeEach('If exists, delete test helper file before each test runs', async () => {
-            await deleteIfExists(testPath);
+        beforeEach('stub the fs.writeFile method with no behaviour', () => {
+            writeFileStub = sinon.stub(fs, 'writeFile');
         });
 
-        after('If exists, delete test helper file after all tests have run', async () => {
-            await deleteIfExists(testPath);
+        afterEach('remove the fs.writeFile stub (from our default sandbox)', () => {
+            writeFileStub.restore();
         });
-        
-        describe('when the file path does not exist already', () => {
-            it('creates a new file with specified data', async () => {
-                const data = 'blah';
-                await createStore(testPath, data);
+
+        it('calls fs.writeFile with the correct filepath and data', async () => {
+            writeFileStub.yields();
+            
+            await createStore(fs, testPath, data);
+            
+            expect(writeFileStub.calledOnce).to.be.true;
+            
+            expect(writeFileStub.getCall(0).args[0]).to.equal(testPath);
+            expect(writeFileStub.getCall(0).args[1]).to.equal(data);
+            expect(writeFileStub.getCall(0).args[2]).to.be.a('function');
+        });
+
+        describe('when fs.writeFile is successful', () => {
+            it('returns a resolved promise with no defined value', async () => {
+                writeFileStub.yields();
                 
-                const store = await readStore(testPath);
-                expect(store).to.equal(data);
+                const returnValue = await createStore(fs, testPath, data);
+                expect(returnValue).to.equal(undefined);
             });
         });
+        
+        describe('when fs.writeFile is unsuccessful', () => {
+            it('returns a rejected promise with the error', async () => {
+                const fakeError = new Error('writeFile failed');
+                
+                writeFileStub.yields(fakeError);
 
-        describe('when a file path already exists', () => {
-            it('overwrites the existing file with specified data', async () => {
-                const existingData = 'existing data';
-                await createStore(testPath, existingData);
-
-                const newerData = 'newer data';
-                await createStore(testPath, newerData);
-
-                const store = await readStore(testPath);
-                expect(store).to.equal(newerData);
+                try {
+                    await createStore(fs, testPath, data);
+                } catch (error) {
+                    expect(error).to.eql(fakeError);
+                }
             });
         });
     });
